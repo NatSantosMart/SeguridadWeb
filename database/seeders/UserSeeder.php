f39b43e76205ea0f040f12aaa6ca60949b3d16e7
NatSantosMart <natsantosmartinez@gmail.com>
2024-03-09 11:27:00 +0100

    Seeder paar user

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,27 @@
+<?php
+
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class UserSeeder extends Seeder
+{
+    public function run()
+    {
+        // Sample client data
+        $clients = [
+            [
+                'name' => 'Carla',
+                'surnames' => 'López Galán',
+                'dni' => '24544343E',
+                'email' => 'seguridadweb@campusviu.es',
+                'password' => bcrypt('S3gur1d4d?W3b'),
+                'IBAN' => 'ES9121000418450200051332',
+            ],
+
+        ];
+        DB::table('users')->insert($clients);
+    }
+}
